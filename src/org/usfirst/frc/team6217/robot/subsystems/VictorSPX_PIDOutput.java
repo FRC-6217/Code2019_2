a25e268291ddb383dd7e40486b15ae69efcd23e7
isaia <isaia@172.21.62.241>
2019-01-06 16:48:07 -0600

    Portion 1 of PIDOutput VictorSPX Wrapper class

---
+++ src/org/usfirst/frc/team6217/robot/subsystems/VictorSPX_PIDOutput.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team6217.robot.subsystems;
+
+import edu.wpi.first.wpilibj.PIDOutput;
+
+public class VictorSPX_PIDOutput implements PIDOutput{
+	private VictorSPX motor;
+	
+	public void pidWrite(double output) {
+		
+	}
+}
